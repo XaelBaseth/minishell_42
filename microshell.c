af93be5525e343f2aeee2b46069321a8f7694f96
Christian Pothin <cpothin@c2r3p3.42lehavre.fr>
2023-07-27 15:02:37 +0200

    push nothing

--- microshell.c
+++ microshell.c
@@ -5,19 +5,11 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <unistd.h>
-#include <signal.h>
 
 /*
 Run the program with < gcc microshell.c -lreadline >
 Quit the program with Ctrl + C
 */
-
-long long	g_exit_status;
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
 
 int cd(char *path)
 //As this command is not a system program, it is executed
@@ -52,40 +44,6 @@ char	**get_input(char *input)
 	return command;
 }
 
-void	child_sig(int signum)
-{
-	if (signum == SIGINT)
-	{
-		ft_putchar_fd('\n', STDOUT_FILENO);
-		g_exit_status = 130;
-		exit(130);
-	}
-}
-
-void	dismiss_sig(int	signum)
-{
-	if (signum == SIGINT)
-	{
-		ft_putchar_fd('\n', STDOUT_FILENO);
-		rl_on_new_line();
-		rl_replace_line("", 0);
-		rl_redisplay();
-		g_exit_status = 130;
-	}
-}
-
-void	config_sig(void)
-{
-	struct sigaction sa;
-
-	sa.sa_handler = &dismiss_sig;
-	sa.sa_flags = SA_RESTART;
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGINT);
-	sigaction(SIGINT, &sa, NULL);
-	signal(SIGQUIT, SIG_IGN);
-}
-
 int	main(void)
 {
 	char **command;
@@ -95,8 +53,7 @@ int	main(void)
 
 	while (1)
 	{
-		config_sig();
-		input = readline("\033[32muWushell$\033[0m ");
+		input = readline("\033[32mmicroshell$\033[0m ");
 		add_history(input);
 		command = get_input(input);
 		if (!command[0]) //Handle empty commands
