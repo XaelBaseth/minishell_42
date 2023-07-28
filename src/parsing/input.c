52cc40f309d2263e2e3cb5e40e671536eb26d561
Christian Pothin <cpothin@c2r3p3.42lehavre.fr>
2023-07-28 17:29:33 +0200

    starting parsing...

--- src/parsing/input.c
+++ src/parsing/input.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   input.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acharlot <acharlot@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cpothin <cpothin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 07:41:55 by acharlot          #+#    #+#             */
-/*   Updated: 2023/07/28 08:40:53 by acharlot         ###   ########.fr       */
+/*   Updated: 2023/07/28 17:29:08 by cpothin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,28 @@
 //					- get operator and applies it.
 //	GLHF :)
 //}
+
+// ne prend pas encore en compte si les parentheses sont dans des guillemets...
+bool check_brackets(char *raw_input)
+{
+	int	i;
+	int	open_brackets;
+	int	close_brackets;
+
+	i = -1;
+	open_brackets = 0;
+	close_brackets = 0;
+	while (raw_input[++i])
+	{
+		if (raw_input[i] == '(')
+			open_brackets++;
+		else if (raw_input[i] == ')')
+			close_brackets++;
+	}
+	if (open_brackets == close_brackets)
+		return (true);
+	return (false);
+}
 
 /*	Returns the input of the user after validating it. */
 char	*get_input(void)
