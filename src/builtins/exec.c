37c41c24808f2902dbc4494a886a05da120f9b72
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2023-08-14 13:54:11 +0200

    Help

--- src/builtins/exec.c
+++ src/builtins/exec.c
@@ -6,7 +6,7 @@
 /*   By: acharlot <acharlot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 08:17:00 by acharlot          #+#    #+#             */
-/*   Updated: 2023/08/14 09:08:37 by acharlot         ###   ########.fr       */
+/*   Updated: 2023/08/14 13:14:20 by acharlot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,31 +17,32 @@
 static void	execute_in_path(t_data *data)
 {
 	int	i;
+	int	executed;
 
-	i = 0;
-	print_path(data);
-	while (i < data->nb_path)
+	i = -1;
+	executed = 0;
+	while (++i < data->nb_path)
 	{
-		if (!access(data->arr_path[i].path, F_OK))
+		if (access(data->arr_path[i].path, X_OK) == 0)
 		{
-			ft_printf("Looking for %s in %s.\n", data->input,
-				data->arr_path[i].path);
-			char *exec_args[] = {data->arr_path[i].path, data->input, NULL};
-			ft_printf("exec_args:\n");
-			for (int j = 0; exec_args[j] != NULL; j++) {
+			char *exec_args[] = {data->input, NULL};
+			ft_printf("exec_args:");
+			for (int j = 0; exec_args[j] != NULL; j++)
 				ft_printf("  %d: %s\n", j, exec_args[j]);
-			}
-			ft_printf("Return of execve: %d\n", execve(data->arr_path[i].path,
-				exec_args, data->envp));
-			execve(data->arr_path[i].path, exec_args, data->envp);
-			perror("execve");
-			panic(EXEC_ERR);
+			ft_printf("Trying to execute: %s\n", data->arr_path[i].path);
+			int exec_result = execve(data->arr_path[i].path, exec_args, data->envp);
+			if (exec_result == -1)
+				perror("execve");
+			else
+				executed = 1;
 		}
-		else
-			i++;
 	}
-	ft_printf("%s not found in any directories in PATH\n", data->input);
+	if (!executed)
+		ft_printf("%s not found in any directories in PATH\n", data->input);
+
 }
+
+
 /*	Check if the command inputed is either part of the added built-ins
 	function or part of the PATH functions. */
 void	execute_cmd(t_data *data)
