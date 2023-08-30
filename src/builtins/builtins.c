9e97f09ae6a2e5cc11e08593b3ea3d656391b285
Axel Charlot <acharlot@c2r4p4.42lehavre.fr>
2023-08-30 08:29:45 +0200

    OK

--- src/builtins/builtins.c
+++ src/builtins/builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: axel <axel@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: acharlot <acharlot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 07:41:52 by acharlot          #+#    #+#             */
-/*   Updated: 2023/08/21 14:26:52 by axel             ###   ########.fr       */
+/*   Updated: 2023/08/30 08:28:10 by acharlot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ bool	builtins(t_args *input, t_data *data)
 		do_echo(data);
 	else if (streq(input->argv[0], "pwd"))
 		get_pwd(data);
-	else if (line_is(data, "exit"))
+	else if (streq(input->argv[0], "exit"))
 		return (do_exit(data));
 	else
 		return (false);
